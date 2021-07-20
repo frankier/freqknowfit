487953efd985bbcb62a4270d8b75109ce6df2168
Frankie Robertson <frankie@robertson.name>
2021-07-20 14:18:41 +0300

    Add script to compare aics between different models

---
+++ freqknowfit/parametric/compare_aics.py
@@ -0,0 +1,24 @@
+import click
+import pandas
+
+
+@click.command()
+@click.argument("measure")
+@click.argument("treat1", type=click.Path(exists=True))
+@click.argument("treat2", type=click.Path(exists=True))
+def main(measure, treat1, treat2):
+    df1 = pandas.read_parquet(treat1)
+    df2 = pandas.read_parquet(treat2)
+    aics1 = df1[measure].to_numpy()
+    aics2 = df2[measure].to_numpy()
+    if len(aics1) != len(aics2):
+        # WARN
+        trunk_len = min(len(aics1), len(aics2))
+        aics1 = aics1[:trunk_len]
+        aics2 = aics2[:trunk_len]
+    print("Treatment 1 better {:.2f}".format(100 * (aics1 > aics2).mean()))
+    print("Treatment 2 better {:.2f}".format(100 * (aics2 > aics1).mean()))
+
+
+if __name__ == "__main__":
+    main()
